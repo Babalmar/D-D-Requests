5f1a59a385e89e7cd82627683ba9a3430567cb48
Marcin <55028490+Babalmar@users.noreply.github.com>
2020-06-04 12:44:14 +0200

    Add files via upload
    Initial versions (without function definitions).

---
+++ monsters.py
@@ -0,0 +1,19 @@
+import requests
+import random
+
+response2 = requests.get('http://www.dnd5eapi.co/api/monsters')
+a2 = response2.json()
+
+lista2 = a2['results']
+
+c = len(lista2) - 1
+monsters = []
+for item in lista2:
+    monsters.append(lista2[c]['index'])
+    c -= 1
+
+random_monster = random.choice(monsters)
+response3 = requests.get('http://www.dnd5eapi.co/api/monsters/'+ random_monster)
+d = response3.json()
+print('Name: {}, Type: {}, Hit points: {}, Hit dice: {}, Armor class: {}'.format(d['name'], d['type'], d['hit_points'], d['hit_dice'], d['armor_class']))
+
---
+++ spells.py
@@ -0,0 +1,37 @@
+import requests
+import random
+
+response = requests.get('http://www.dnd5eapi.co/api/spells')
+spells_response = response.json()
+
+lista = spells_response['results']
+
+spells_id = len(lista) - 1
+spells = []
+for item in lista:
+    spells.append(lista[spells_id]['index'])
+    spells_id -= 1
+
+random_spell = random.choice(spells)
+response1 = requests.get('http://www.dnd5eapi.co/api/spells/'+ random_spell)
+a1 = response1.json()
+
+print('Name: {}\nDescription: {}'.format(a1['name'], ' '.join(a1['desc'])))
+if 'higher_level' in a1:
+    print('Higher level: {}'.format(' '.join(a1['higher_level'])))
+print('Range: {}\nComponents: {}'.format(a1['range'], ' '.join(a1['components'])))
+if 'material' in a1:
+    print('Materials: {}'.format(a1['material']))
+print('Casting time: {}\nLevel: {}\nSchool: {}'.format(a1['casting_time'], a1['level'], a1['school']['name']))
+
+lista = a1['classes']
+
+classes_id = len(lista) - 1
+classes = []
+for item in lista:
+    classes.append(lista[classes_id]['name'])
+    classes_id -= 1
+    
+print('Classes:')
+for profession in sorted(classes):
+    print(profession)
