f44b2a2ec24e85a85fd11973e2a658cde7d520e4
Marcin <marcin.babalski@gmail.com>
2022-12-27 16:27:09 +0100

    Update ability_scores.py
    ability score request rewritten as function

--- ability_scores.py
+++ ability_scores.py
@@ -1,29 +1,32 @@
 import requests
 import random
 
-response = requests.get('http://www.dnd5eapi.co/api/ability-scores')
-equipment_response = response.json()
+def abilities():
+    response = requests.get('http://www.dnd5eapi.co/api/ability-scores')
+    equipment_response = response.json()
 
-lista = equipment_response['results']
+    lista = equipment_response['results']
 
-equipment_id = len(lista) - 1
-equipment = []
-for item in lista:
-    equipment.append(lista[equipment_id]['index'])
-    equipment_id -= 1
+    equipment_id = len(lista) - 1
+    equipment = []
+    for item in lista:
+        equipment.append(lista[equipment_id]['index'])
+        equipment_id -= 1
 
-random_ability_score = random.choice(equipment)
-response1 = requests.get('http://www.dnd5eapi.co/api/ability-scores/'+ random_ability_score)
-a1 = response1.json()
+    random_ability_score = random.choice(equipment)
+    response1 = requests.get('http://www.dnd5eapi.co/api/ability-scores/'+ random_ability_score)
+    a1 = response1.json()
 
-lista = a1['skills']
+    lista = a1['skills']
 
-skill_id = len(lista) - 1
-skills = []
-for item in lista:
-    skills.append(lista[skill_id]['name'])
-    skill_id -= 1
+    skill_id = len(lista) - 1
+    skills = []
+    for item in lista:
+        skills.append(lista[skill_id]['name'])
+        skill_id -= 1
     
-print('Name: {}\nDescription: {}\nSkills:'.format(a1['full_name'], ' '.join(a1['desc'])))
-for skill in sorted(skills):
-    print(skill)
+    print('Name: {}\nDescription: {}\nSkills:'.format(a1['full_name'], ' '.join(a1['desc'])))
+    for skill in sorted(skills):
+        print(skill)
+
+abilities()
