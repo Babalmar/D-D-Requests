d969007078d2ee789c7aee14dbd7b6827b144e8e
Marcin <marcin.babalski@gmail.com>
2022-12-28 16:14:27 +0100

    Update main.py
    Wrapper for all requests

---
+++ main.py
@@ -0,0 +1,18 @@
+import ability_scores as absc
+import spells
+import monsters
+import classes
+import equipment
+
+def dd_requests():
+    text = '''Choose what information you want to fetch?\n
+    1 - Ability scores\n
+    2 - Classes\n
+    3 - Equipment\n
+    4 - Spells\n
+    5 - Monsters
+'''
+    print(text)
+
+
+dd_requests()
