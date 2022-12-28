b9331b6e2f577a96a2ec55d3d1b83e89246fa25e
Marcin <marcin.babalski@gmail.com>
2022-12-28 20:22:53 +0100

    Adding  modules for the requests

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import ability_scores as absc
-import spells
-import monsters
+import ability_scores
 import classes
 import equipment
+import spells
+import monsters
 
 def dd_requests():
     text = '''Choose what information you want to fetch?\n
@@ -10,9 +10,13 @@ def dd_requests():
     2 - Classes\n
     3 - Equipment\n
     4 - Spells\n
-    5 - Monsters
-'''
-    print(text)
-
-
+    5 - Monsters\n'''
+    
+    option = input(text)
+    
+    if int(option) == 1:
+        monsters.monster()
+        
+    
+    
 dd_requests()
